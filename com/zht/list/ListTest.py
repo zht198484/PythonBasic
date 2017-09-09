7065d198a5090d67946f6c72087f40f1b8f26651
zht198484 <zht198484@163.com>
2017-09-09 16:17:10 +0800

    add indent flag to control whether to print tab*level before actual content in list

--- com/zht/list/ListTest.py
+++ com/zht/list/ListTest.py
@@ -21,12 +21,15 @@ for i in seq:
 print isinstance(seq, list)
 
 
-def print_seq(the_seq, level=0):
-    for i in the_seq:
-        if isinstance(i, list):
-            print_seq(i, level+1)
+def print_seq(the_seq, indent=False, level=0):
+    for list_item in the_seq:
+        if isinstance(list_item, list):
+            print_seq(list_item, indent, level + 1)
         else:
-            print '\t'*level, i
+            tab_str = ''
+            if indent:
+                tab_str = '\t' * level
+            print tab_str, list_item
 
 
-print_seq(seq)
+print_seq(seq, True)
