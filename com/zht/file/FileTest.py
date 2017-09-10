d7e50ad7f5f592c6ffd40865fc2e67cf0bcf37f2
zht198484 <zht198484@163.com>
2017-09-10 21:49:15 +0800

    add FileTest with usages of 'with as' and 'try except error as e' and split with max depth after verify if separator exists

--- com/zht/file/FileTest.py
+++ com/zht/file/FileTest.py
@@ -6,6 +6,17 @@ os.chdir("../../../")
 
 print os.getcwd()
 
-with open("111.txt") as lines:
-    for line in lines:
-        print line
+if os.path.exists("111.txt"):
+    with open("111.txt") as lines:
+        for line in lines:
+            print line
+            split = line.split(',')
+            print type(split)
+            print len(split)
+            try:
+                if line.find(',') > 0:
+                    (part0, part1) = line.split(',', 1)
+                    print 'part0 = ', part0, ',part1 = ', part1
+            except ValueError as e:
+                print e
+                print 'Unable to use comma  to split {} due to error {} '.format(line, str(e))
