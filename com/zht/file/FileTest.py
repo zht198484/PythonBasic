e27f83ce4f0eee0c39e00587641d8ec4a961b105
zht198484 <zht198484@163.com>
2017-09-11 23:08:20 +0800

    Write list to multiple files open using 'with' which is context management protocol

--- com/zht/file/FileTest.py
+++ com/zht/file/FileTest.py
@@ -6,6 +6,8 @@ os.chdir("../../../")
 
 print os.getcwd()
 
+odd_list = []
+even_list = []
 if os.path.exists("111.txt"):
     with open("111.txt") as lines:
         for line in lines:
@@ -17,6 +19,19 @@ if os.path.exists("111.txt"):
                 if line.find(',') > 0:
                     (part0, part1) = line.split(',', 1)
                     print 'part0 = ', part0, ',part1 = ', part1
+                    if int(part0) % 2 == 0:
+                        even_list.append(part1)
+                    else:
+                        odd_list.append(part1)
             except ValueError as e:
                 print e
                 print 'Unable to use comma  to split {} due to error {} '.format(line, str(e))
+
+print odd_list
+print even_list
+
+with open('odd.txt', 'w') as odd_file, open('even.txt', 'w') as even_file:
+    for odd in odd_list:
+        odd_file.write(odd)
+    for even in even_list:
+        even_file.write(even)
