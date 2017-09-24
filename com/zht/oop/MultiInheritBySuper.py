7868fce09c46abbd551950ca20aeb7ad14d61d60
zht198484 <zht198484@163.com>
2017-09-24 23:13:35 +0800

    Python inherit examples implemented by normal and super way

---
+++ com/zht/oop/MultiInheritBySuper.py
@@ -0,0 +1,36 @@
+class A1(object):
+    def __init__(self):
+        print 'Enter A'
+        print 'Leave A'
+
+
+class B1(A1):
+    def __init__(self):
+        print 'Enter B'
+        super(B1, self).__init__()
+        print 'Leave B'
+
+
+class C1(A1):
+    def __init__(self):
+        print("Enter C")
+        super(C1, self).__init__()
+        print("Leave C")
+
+
+class D1(A1):
+    def __init__(self):
+        print("Enter D")
+        super(D1, self).__init__()
+        print("Leave D")
+
+
+class E1(B1, C1, D1):
+    def __init__(self):
+        print("Enter E")
+        super(E1, self).__init__()
+        print("Leave E")
+
+
+print E1.mro()
+E1()
